de8a9680ca6d3c4e1b2fdbdf5d6c5c7f0326f582
arhgi128 <arhgi128@gmail.com>
2017-07-19 16:53:04 +0200

    added services for directory and directoryCategory

---
+++ Code/MaVille/MaVilleBack/src/main/java/com/maville/back/service/implementations/DirectoryServiceImpl.java
@@ -0,0 +1,62 @@
+package com.maville.back.service.implementations;
+
+import java.util.List;
+
+import com.maville.back.dto.DirectoryCategoryDTO;
+import com.maville.back.dto.DirectoryDTO;
+import com.maville.back.service.interfaces.DirectoryService;
+
+public class DirectoryServiceImpl implements DirectoryService {
+
+	@Override
+	public DirectoryDTO getDirectoryById(int id) {
+		DirectoryDTO directory = null;
+		// TODO
+		return directory;
+	}
+
+	@Override
+	public List<DirectoryDTO> getAllDirectory() {
+		List<DirectoryDTO> listDirectory = null;
+		// TODO
+		return listDirectory;
+	}
+
+	@Override
+	public List<DirectoryDTO> getDirectoryByName(String name) {
+		List<DirectoryDTO> listDirectory = null;
+		// TODO
+		return listDirectory;
+	}
+
+	@Override
+	public void addDirectory(DirectoryDTO directory) {
+		// TODO 
+	}
+
+	@Override
+	public void delecteDirectory(DirectoryDTO directory) {
+		// TODO 
+	}
+
+	@Override
+	public void updateDirectory(DirectoryDTO directory) {
+		// TODO 
+
+	}
+
+	@Override
+	public List<DirectoryDTO> getDirectoryByCategory(DirectoryCategoryDTO category) {
+		List<DirectoryDTO> listDirectory = null;
+		// TODO 
+		return listDirectory;
+	}
+
+	@Override
+	public List<DirectoryDTO> getDirectoryByCoord(String lat, String lng, double rayon) {
+		List<DirectoryDTO> listDirectory = null;
+		// TODO 
+		return listDirectory;
+	}
+
+}
