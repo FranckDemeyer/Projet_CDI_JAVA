7aad3b0fd66d758654a3564841172c56f7a8dc7a
Franck Demeyer <franck.demeyer@free.fr>
2017-07-20 15:04:56 +0200

    Update AccountService.java

--- Code/MaVille/MaVilleBack/src/main/java/com/maville/back/service/interfaces/AccountService.java
+++ Code/MaVille/MaVilleBack/src/main/java/com/maville/back/service/interfaces/AccountService.java
@@ -7,15 +7,9 @@ import com.maville.back.dto.AccountDTO;
 public interface AccountService {
 	AccountDTO getAccountById(int id);
 	List<AccountDTO> getAllAccount();
-<<<<<<< HEAD
-	void addAccount(AccountDTO account) throws Exception;
-	void deleteAccount(AccountDTO account);
-	AccountDTO updateAccount(AccountDTO account) throws Exception;
-=======
 	List<AccountDTO> getAllProfessional();
 	List<AccountDTO> getAllAdmin();
 	void addAccount(AccountDTO account) throws Exception;
 	void deleteAccount(AccountDTO account);
 	void updateAccount(AccountDTO account) throws Exception;
->>>>>>> 0a64aa8b9a45cafc693b9a88a4b1a11938b58893
 }
