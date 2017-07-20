edc7840e223a6488f5d5284cc8b7d14322d0d024
Franck Demeyer <franck.demeyer@free.fr>
2017-07-20 15:05:32 +0200

    Update DirectoryCategoryService.java

--- Code/MaVille/MaVilleBack/src/main/java/com/maville/back/service/interfaces/DirectoryCategoryService.java
+++ Code/MaVille/MaVilleBack/src/main/java/com/maville/back/service/interfaces/DirectoryCategoryService.java
@@ -3,15 +3,6 @@ package com.maville.back.service.interfaces;
 import java.util.List;
 
 import com.maville.back.dto.DirectoryCategoryDTO;
-<<<<<<< HEAD
-
-public interface DirectoryCategoryService {
-	DirectoryCategoryDTO getDirectoryCategoryById(int id);
-	List<DirectoryCategoryDTO> getAllDirectoryCategory();
-	void addDirectoryCategory(DirectoryCategoryDTO directoryCategory);
-	void deleteDirectoryCategory(DirectoryCategoryDTO directoryCategory);
-	void updateDirectoryCategory(DirectoryCategoryDTO directoryCategory);
-=======
 import com.maville.back.dto.DirectoryDTO;
 
 public interface DirectoryCategoryService {
@@ -30,5 +21,4 @@ public interface DirectoryCategoryService {
 
 	public List<DirectoryDTO> getDirectoriesByCategory(int idCategory);
 
->>>>>>> 0a64aa8b9a45cafc693b9a88a4b1a11938b58893
 }
